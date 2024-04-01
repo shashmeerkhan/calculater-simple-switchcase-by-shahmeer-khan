b2cccf5f82b6c9af98f2afa0afc0cf11bd257114
shashmeerkhan <142727342+shashmeerkhan@users.noreply.github.com>
2024-04-01 03:42:53 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold("\n \t This is CLI base simple calculator \n"));
 const answer = await inquirer.prompt([
     {
         type: "number",
-        message: "Enter your first number",
+        message: chalk.yellow("Enter your first number"),
         name: "Num1",
     },
     {
         type: "number",
-        message: "Enter your second number",
+        message: chalk.yellow("Enter your second number"),
         name: "Num2",
     },
     {
         type: "list",
         message: "Choose an operation",
         name: "operator",
-        choices: ["+", "-", "*", "/"],
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
     },
 ]);
 // step 02
@@ -22,19 +25,18 @@ const { Num1, Num2, operator } = answer;
 //step 03
 let result;
 switch (operator) {
-    case "+":
+    case "Addition":
         result = Num1 + Num2;
         break;
-    case "-":
+    case "ubtraction":
         result = Num1 - Num2;
         break;
-    case "*":
+    case "Multiplication":
         result = Num1 * Num2;
         break;
-    case "/":
+    case "Division":
         result = Num1 / Num2;
         break;
-    default:
+    default: console.log(chalk.red("invalid operator"));
 }
-console.log("invalid operator");
-console.log(`${Num1} ${operator} ${Num2} = ${result}`);
+console.log(chalk.green.bold(`\n \t ${Num1} ${operator} ${Num2} = ${result}\n`));
--- index.ts
+++ index.ts
@@ -1,23 +1,24 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
+console.log(chalk.blue.bold("\n \t This is CLI base simple calculator \n"));
 
 const answer = await inquirer.prompt([
     {
         type: "number",
-        message: "Enter your first number",
+        message: chalk.yellow("Enter your first number"),
         name: "Num1",
     },
     {
         type: "number",
-        message: "Enter your second number",
+        message: chalk.yellow("Enter your second number"),
         name: "Num2",
     },
     {
         type: "list",
         message: "Choose an operation",
         name: "operator",
-        choices: ["+", "-", "*", "/"],
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
     },
 ]);
 
@@ -26,20 +27,20 @@ const {Num1,Num2,operator}=answer
 //step 03
 let result
 switch (operator) {
-    case "+":
+    case "Addition":
         result=Num1+Num2
         break;
-case "-":
+case "ubtraction":
     result=Num1-Num2
     break;
-    case "*":
+    case "Multiplication":
         result=Num1*Num2
         break;
-    case "/":
+    case "Division":
         result=Num1/Num2
         break;
-    default:}
-console.log("invalid operator");
-console.log(`${Num1} ${operator} ${Num2} = ${result}`);
+    default:console.log(chalk.red("invalid operator"));}
+
+console.log(chalk.green.bold(`\n \t ${Num1} ${operator} ${Num2} = ${result}\n`));
 
 
